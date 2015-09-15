521a55d041fafe6a48196e7953aa93eb92c04765
Chris Bishop <domingobishop@gmail.com>
2015-09-15 12:44:02 +0100

    Added content

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -1,15 +1,97 @@
-
-
-
-
-
-<section class="row">
-    <div class="col starts-at-full clr holding-box">
+<div class="row">
+    <div class="col ends-at-one-third clr holding-box">
         <div class="heading-holding-banner">
-            <h2><span><span>[Page title]</span></span></h2>
+            <h2><span><span>Somerset archives</span></span></h2>
         </div>
         <div class="breather">
-            <p>[Page content]</p>
+            <p>We exist to find, preserve and make available written records of Somerset's people and communities.  We are based at the Somerset Heritage Centre in Taunton.</p>
+            <p>Somerset Archives and Local Studies offers:</p>
+            <ul class="child-full">
+                <li>
+                    Free access for you to do your <a href="">own research</a>
+                </li>
+                <li>
+                    <a href="">Accepting documents</a> relating to Somerset
+                </li>
+                <li>
+                    An archives <a href="">online catalogue</a>
+                </li>
+                <li>
+                    A <a href="">Research Service</a> if you can't visit in person
+                </li>
+            </ul>
         </div>
     </div>
-</section>
+    <div class="col ends-at-one-third clr holding-box">
+        <div class="heading-holding-banner">
+            <h2><span><span>Planning a visit</span></span></h2>
+        </div>
+        <div class="breather-all clr">
+            <ul class="child-full">
+                <li>
+                    <a href="">Opening hours</a>
+                </li>
+                <li>
+                    <a href="">Directions</a>
+                </li>
+                <li>
+                    <a href="">Appointments</a>
+                </li>
+                <li>
+                    <a href="">Consulting documents</a>
+                </li>
+                <li>
+                    <a href="">Consulting photographs</a>
+                </li>
+                <li>
+                    <a href="">Ticket system</a>
+                </li>
+                <li>
+                    <a href="">Searchrooms</a>
+                </li>
+                <li>
+                    <a href="">Taking photographs</a>
+                </li>
+                <li>
+                    <a href="">Facilities available</a>
+                </li>
+                <li>
+                    <a href="">Charges</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="col ends-at-one-third clr holding-box research-guides">
+        <div class="heading-holding-banner">
+            <h2><span><span>Services we provide</span></span></h2>
+        </div>
+        <div class="breather-all clr">
+            <ul class="child-full">
+                <li>
+                    <a href="">Visiting the Heritage Centre</a>
+                </li>
+                <li>
+                    <a href="">Donors and Depositors</a>
+                </li>
+                <li>
+                    <a href="">Research Service</a>
+                </li>
+                <li>
+                    <a href="">Education and Learning</a>
+                </li>
+                <li>
+                    <a href="">Archive Services to North Somerset</a>
+                </li>
+                <li>
+                    <a href="">Conservation</a>
+                </li>
+                <li>
+                    <a href="">Talks and Tours</a>
+                </li>
+                <li>
+                    <a href="">Reprographics</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
